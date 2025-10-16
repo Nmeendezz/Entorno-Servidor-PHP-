eb4b0ce31bc5ac8605bcef9e3d8d0c505dcc72d1
Nmeendezz <nicolasmarin2006@hotmail.com>
2025-10-17 00:00:24 +0200

    tabla

--- prac1NM/prac1NM.php
+++ prac1NM/prac1NM.php
@@ -5,6 +5,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        div {
+            text-align: center;
+            align-items: center;
+            width: 80%;
+            margin: 0 auto;
+            justify-items: center;
+        }
+
+        h1 {
+            background-color: #465055;
+            color: white;
+            padding-top: 10px;
+            padding-bottom: 10px;
+            width: 100%;
+        }
+
+        table,
+        td,
+        th {
+            border: solid 1px;
+            border-collapse: collapse;
+            width: 100%;
+
+        }
+
+        table {
+            border-radius: 20px;
+        }
+
+        td,
+        th {
+            padding: 20px;
+            width: 30px;
+        }
+
+        .blue {
+            background-color: #cdebf0;
+        }
+
+        .green {
+            background-color: #d2ebd7;
+        }
+    </style>
 </head>
 
 <body>
@@ -139,28 +183,64 @@
     echo "<br>";
     echo $min;
     echo "<br>";
-    
+
     //Dia con mayor variacion termica
     $diaMax = 0;
     foreach ($temps as $ciudad => $info) {
-        for( $i = 0; $i < count($info); $dia++) {
-            if($info[$i] > $diaMax){
+        for ($i = 0; $i < count($info); $i++) {
+            if ($info[$i] > $diaMax) {
                 $diaMax = $info[$i];
             }
         }
     }
     echo $diaMax;
+    echo "<br><br>";
 
     //Media por ciudad
     $sum = 0;
+    $cont = 0;
     foreach ($temps as $ciudad => $info) {
         foreach ($info as $temperatura) {
             $sum += $temperatura;
-            $info["media"] = $sum / count($info);
+            $cont++;
         }
+        $temps[$ciudad]['media'] = $sum / $cont;
     }
-    var_dump($temps);
+    foreach ($temps as $ciudad => $info) {
+        echo $temps[$ciudad]['media'];
+        echo "<br>";
+    }
+
     ?>
+    <div>
+        <h1>Temperaturas de ciudades por dia (Cº)</h1>
+        <table>
+            <tr>
+                <th>Ciudad/Dia</th>
+                <th>Dia 1</th>
+                <th>Dia 2</th>
+                <th>Dia 3</th>
+                <th>Dia 4</th>
+                <th>Dia 5</th>
+                <th class="green">Dia 6</th>
+                <th>Media</th>
+            </tr>
+            <?php
+            foreach ($temps as $ciudad => $info) {
+                echo "<tr>";
+                echo '<th class="blue">' . $ciudad . '</th>';
+                foreach( $info as $temperatura ) {
+                    
+                    echo "<td> $temperatura </td>";
+                }
+                echo "</tr>";
+            }
+
+            ?>
+        </table>
+    </div>
+
+
 </body>
 
 </html>
